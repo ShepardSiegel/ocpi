76efe65c06a3f5d6ba23eea837bf02fd0c9e920c
Shepard Siegel <shepard.siegel@atomicrules.com>
2011-06-21 17:10:10 -0400

    fix typo

--- libsrc/hdl/ocpi/fpgaTop_kc705.v
+++ libsrc/hdl/ocpi/fpgaTop_kc705.v
@@ -10,7 +10,7 @@ module fpgaTop (
   //input  wire [ 3:0]  pcie_rx,       // PCIe lanes...
   //output wire [ 3:0]  pcie_tx,
   input  wire [ 7:0]  usr_sw,        // dip-switches
-  output wire [ 7:0]  led            // leds
+  output wire [ 7:0]  led,           // leds
   output wire [31:0]  debug          // debug
 );
 
