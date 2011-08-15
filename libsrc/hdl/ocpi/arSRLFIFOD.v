1e08e69f6f13c39a81ff9108c01f5c8055490ade
Shepard Siegel <shepard.siegel@atomicrules.com>
2011-08-15 13:02:01 -0400

    transpose length and width for RB1/RB2 SRL inference

--- libsrc/hdl/ocpi/arSRLFIFOD.v
+++ libsrc/hdl/ocpi/arSRLFIFOD.v
@@ -6,6 +6,7 @@
 // 2009-11-14 ssiegel Add D flop at back end for greater Fmax
 // 2011-06-17 ssiegel Update logic to follow non-registered version
 // 2011-07-28 hyzeng  Changed ENQ action so that XST for both V5 and V6 would infer SRL
+// 2011-08-15 ssiegel Reverse index of SRL FIFO register to get SRL inference
 
 module arSRLFIFOD (CLK,RST_N,ENQ,DEQ,FULL_N,EMPTY_N,D_IN,D_OUT,CLR);
 
@@ -23,12 +24,11 @@ module arSRLFIFOD (CLK,RST_N,ENQ,DEQ,FULL_N,EMPTY_N,D_IN,D_OUT,CLR);
   input[width-1:0]  D_IN;
   output[width-1:0] D_OUT;
 
-  reg[l2depth-1:0]  pos;             // head position
-  reg[depth-1:0]    dat[width-1:0];  // SRL FIFO
-  reg[width-1:0]    dreg;            // Ouput register
-  reg               sempty, sfull, dempty;
-  wire              sdx;             // SRL-DEQ and D-ENQ 
-  wire[l2depth-1:0] pos_minus_one = pos - 1;
+  reg[l2depth-1:0]  pos;                    // Head position
+  reg[width-1:0]    dat[depth-1:0];         // SRL FIFO
+  reg[width-1:0]    dreg;                   // Ouput register
+  reg               sempty, sfull, dempty;  // Flags
+  wire              sdx;                    // SRL-DEQ and D-ENQ 
 
   integer i;
 
@@ -41,7 +41,10 @@ module arSRLFIFOD (CLK,RST_N,ENQ,DEQ,FULL_N,EMPTY_N,D_IN,D_OUT,CLR);
     end else begin
       if (!ENQ &&  sdx) pos <= pos - 1;
       if ( ENQ && !sdx) pos <= pos + 1;
-      if (ENQ) for(i=0;i<width;i=i+1) dat[i] <= {dat[i][depth-2:0],D_IN[i]};
+      if ( ENQ) begin  // SRL should be inferred here...      
+        for(i=depth-1;i>0;i=i-1) dat[i] <= dat[i-1];
+        dat[0] <= D_IN;
+      end
       sempty <= ((pos==0         && !ENQ) || (pos==1         && (sdx&&!ENQ)));
       sfull  <= ((pos==(depth-1) && !sdx) || (pos==(depth-2) && (ENQ&&!sdx)));
 
@@ -49,8 +52,7 @@ module arSRLFIFOD (CLK,RST_N,ENQ,DEQ,FULL_N,EMPTY_N,D_IN,D_OUT,CLR);
       // appended to the output of the SRL FIFO. An ENQ of of the 1-deep FIFO 
       // must happen with a DEQ of the SRL FIFO, this internal signal is "sdx"
       if (sdx) begin 
-        //dreg   <= dat[pos-1];  // transfer the SRL to the D reg
-        for(i=0;i<width;i=i+1) dreg[i] <= dat[i][pos_minus_one];
+        dreg   <= dat[pos-1];  // transfer the SRL to the D reg
         dempty <= 1'b0;        // dempty becomes False when we load the 1-deep FIFO
       end
       if (DEQ && sempty) begin
