54a4ae339420b7b038f84e38d7363c36a07aa2d8
Shepard Siegel <shepard.siegel@atomicrules.com>
2011-11-03 15:14:40 -0400

    tick define and tick include

---
+++ libsrc/hdl/ocpi/OCInf.v
@@ -0,0 +1,11 @@
+// OCInf.v - OCInf with `define `include paramaterization
+
+`ifdef USE_NDW1
+`include "../../../mkOCInf4B.v"
+`elsif USE_NDW2
+`include "../../../mkOCInf8B.v"
+`elsif USE_NDW4
+`include "../../../mkOCInf16B.v"
+`elsif USE_NDW8
+`include "../../../mkOCInf32B.v"
+`endif
