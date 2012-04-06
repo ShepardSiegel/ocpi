2d625a55fb4f2b0da814bb0643997f3b4333e81a
Shepard Siegel <shepard.siegel@atomicrules.com>
2012-04-06 15:30:32 -0400

    nop, write, read by Ethernet GMAC first functional

--- bin/send_dcp.py
+++ bin/send_dcp.py
@@ -14,12 +14,12 @@ from scapy.all import *
 def main(argv):
   print """Hello from %s""" % (prog_name)
   p = Ether()
-  p.src  = '00:26:E1:01:01:00'
-  p.dst  = '00:0A:35:42:01:00'
-  p.type = 0xF040
-  #p.payload = "\x00\x0A\x00\x00\x0F\x05\x80\x00\x00\x01"
-  p.payload = "\x00\x0E\x00\x00\x1F\x06\x00\x00\x00\x24\x00\x00\x00\x01"
-  p.show()
+  p.src  = '00:26:E1:01:01:00'   # Linux Host Source MAC Address
+  p.dst  = '00:0A:35:42:01:00'   # Xilinx FPGA Dest MAC Address
+  p.type = 0xF040                # EtherType TCP
+  #p.payload = "\x00\x0A\x00\x00\x0F\x05\x80\x00\x00\x01"                    # 10B NOP
+  #p.payload = "\x00\x0E\x00\x00\x1F\x06\x00\x00\x00\x24\x00\x00\x00\x02"    # 14B Write 0x24 with 0x00000002
+  p.payload = "\x00\x0A\x00\x00\x2F\x07\x00\x00\x00\x24"                     # 10B Read 0x24
   print "Sending packet..."
   sendp(p, iface="eth1")
 
