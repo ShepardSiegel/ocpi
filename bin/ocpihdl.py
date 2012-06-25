6cfce78d3c365a81b817ab00443a8c986bcaf974
Shepard Siegel <shepard.siegel@atomicrules.com>
2012-06-25 11:14:57 -0400

    advance ocpihdl.py

--- bin/ocpihdl.py
+++ bin/ocpihdl.py
@@ -11,16 +11,32 @@ dev0 = 'ether:eth1/A0:36:FA:25:3B:81'
 def probe(device):
   cmd = ["./ocpihdl", "probe", device]
   cmdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-  print cmdout
-  return(0)
+  if "found" in cmdout:
+    print 'Found device', device
+    return(0);
+  else:
+    return(cmdout)
+
+def radmin(device, offset):
+  cmd = ["./ocpihdl", "radmin", "-P", device, str(offset)]
+  cmdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  return(int(cmdout, 0))
 
 def wread(device, workerNum, offset):
-  print device
-  cmd = ["sudo", "-E", "./ocpihdl", "wread", "-P", device, str(workerNum), str(offset), "1"]
-  print 'cmd is ', cmd
-  cmdout = subprocess.popen(cmd, stderr=subprocess.STDOUT)
-  print cmdout
-  return(0)
+  cmd = ["./ocpihdl", "wread", "-P", device, str(workerNum), str(offset), "1"]
+  cmdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  return(int(cmdout, 0))
+
+def wadmin(device, offset, wdata):
+  cmd = ["./ocpihdl", "wadmin", "-P", device, str(offset), str(wdata)]
+  cmdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  #return(int(cmdout, 0))
+
+def wwrite(device, workerNum, offset, wdata):
+  cmd = ["./ocpihdl", "wdata", "-P", device, str(workerNum), str(offset), str(wdata)]
+  cmdout = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  #return(int(cmdout, 0))
+
 
 def main(argv):
   if len(argv) != 2:
@@ -30,8 +46,15 @@ where <argfoop> is a valid foop.""" % (prog_name)
     sys.exit(1)
   else:
     print 'argv 1 is ' + argv[1]
-    probe(dev0)
-    wread(dev0, 11, 0x0C)
+    if probe(dev0):
+      print 'Device not Found'. dev0
+      sys.exit(1)
+    wadmin(dev0, 0x20, 0xf00dc0de)
+    wadmin(dev0, 0x24, 0xfeedface)
+    rval = radmin(dev0, 0x20)
+    print hex(rval)
+    rval = radmin(dev0, 0x24)
+    print hex(rval)
     sys.exit(0)
 
 prog_name = os.path.basename(sys.argv[0])
